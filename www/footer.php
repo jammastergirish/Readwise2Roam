bd9fd8356c2d59715cfdd2e7ca7e8a1812298328
Girish Gupta <girish@girish-gupta.com>
2020-04-05 21:18:08 +0530

    Changes copyright date

--- www/footer.php
+++ www/footer.php
@@ -1,6 +1,6 @@
 
 <br><br>
-<p style="font-size: 0.8em;">&copy; 2019 <a href="https://www.girishgupta.com/">Girish Gupta</a></p>
+<p style="font-size: 0.8em;">&copy; 2020 <a href="https://www.girishgupta.com/">Girish Gupta</a></p>
 
 </CENTER>
 </body>
