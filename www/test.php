0bc4b80b62dd2ddf7a89875ad1683a857622435e
Girish Gupta <girish@girish-gupta.com>
2020-04-29 16:05:46 +0530

    Corrects project ID

--- www/test.php
+++ www/test.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$ProjectID = "readwise2roam-275316";
+$ProjectID = "readwise2roam-1";
 
 require_once 'vendor/autoload.php';  
   
