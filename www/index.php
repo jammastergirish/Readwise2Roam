d7dc723ab5e72392cabed3fb73846373fcc6a413
Girish Gupta <girish@girish-gupta.com>
2020-04-29 22:02:56 +0530

    Adds local testing

--- www/index.php
+++ www/index.php
@@ -1,4 +1,21 @@
 <?php
+
+if ($_SERVER['SERVER_PORT']!=8888) // If I were on 8888, I'd be testing.
+{
+  if (substr($_SERVER['HTTP_HOST'],0,3)!="www")
+  {
+    header("Location: https://www.".$_SERVER['HTTP_HOST'].$_SERVER[REQUEST_URI]);
+  }
+}
+else
+{
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
+  error_reporting(E_ALL);
+
+  putenv("GOOGLE_APPLICATION_CREDENTIALS=Readwise2Roam-9bf798a7e7e7.json"); // https://cloud.google.com/storage/docs/reference/libraries#client-libraries-usage-php
+}
+
 switch (@parse_url($_SERVER['REQUEST_URI'])['path'])
 {
     case '/':
